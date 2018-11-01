05b29a10c1ef5ba34a234b2b26912ce5579833f9
Christina Landvytovych <Crofty.land@gmail.com>
2018-11-01 17:46:21 +0200

    RNA transcription fixed

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,10 +1,12 @@
+const DNA_TO_RNA = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U',
+};
+
 export const toRna = (dna) => {
-    //const rna = dna.replace(/./g, nucleotide => DNA_TO_RNA[nucleotide]);
-    const rna = dna
-        .replace('A', 'U')
-        .replace('C', 'G')
-        .replace('G', 'C')
-        .replace('T', 'A');
+    const rna = dna.replace(/./g, nucleotide => DNA_TO_RNA[nucleotide]);
     if (rna.length !== dna.length) {
         // invalid characters in the strand
         throw new Error('Invalid input DNA.');
