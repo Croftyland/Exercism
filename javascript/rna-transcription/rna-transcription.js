382158785d75fe963b82d76f84393053fa2531d0
Christina Landvytovych <Crofty.land@gmail.com>
2018-11-01 17:26:51 +0200

    RNA transcription fixed

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,21 @@
-function toRna(array){
-    myStr = array
-        .replace('A', 'U')
-        .replace('C', 'G')
-        .replace('G', 'C')
-        .replace('T', 'A')
-    return myStr;
-}
+"use strict";
+
+var DnaTranscriber = function() {
+    this.trans = {
+        G: 'C',
+        C: 'G',
+        T: 'A',
+        A: 'U',
+    };
+};
+
+DnaTranscriber.prototype.toRna = function (strand) {
+    return strand.split('').map( x => {
+        if (!this.trans[x]) {
+            throw new Error("Invalid input");
+        }
+        return this.trans[x];
+    }).join('');
+};
+
+module.exports = DnaTranscriber;
