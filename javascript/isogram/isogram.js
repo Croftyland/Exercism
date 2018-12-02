68c22723c41dee67650131e234d5d73e6a168bfe
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-02 13:15:51 +0200

    isogram problem solution

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,5 @@
+export const isIsogram = (string) => {
+    const stringNoSpaceOrHyphen = string.replace(/ |-/g, '');
+    const uniqueLetters = stringNoSpaceOrHyphen.toLowerCase().split('').filter((element, index, self) => self.indexOf(element) === index);
+    return uniqueLetters.length === stringNoSpaceOrHyphen.length;
+};
