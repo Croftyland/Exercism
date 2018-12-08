d76e69a618c56c659007f55711e3205e45878c51
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-08 10:49:47 +0200

    bob problem solution

---
+++ javascript/bob/bob.js
@@ -0,0 +1,19 @@
+const isSilence = message => message.replace(/\s+/g, '') === '';
+const isShouting = message => message.toUpperCase() === message && /[A-Z]/.test(message);
+const isAQuestion = message => message[message.length - 1] === '?';
+
+export const hey = (message) => {
+    if (isSilence(message)) {
+        return 'Fine. Be that way!';
+    }
+    if (isShouting(message)) {
+        if (isAQuestion(message)) {
+            return "Calm down, I know what I'm doing!";
+        }
+        return 'Whoa, chill out!';
+    }
+    if (isAQuestion(message.trim())) {
+        return 'Sure.';
+    }
+    return 'Whatever.';
+};
