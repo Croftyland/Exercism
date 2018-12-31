050eab3778ef8a0a53731484e0a8c01bf43512e7
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-31 23:45:54 +0200

    clock solution P.S Buy 2k18!

---
+++ javascript/clock/clock.js
@@ -0,0 +1,41 @@
+export default function (hour, minute) {
+    const MINUTESPERDAY = 1440;
+    const HOURSPERDAY = 24;
+
+    const clock = {
+        hour,
+        minute: minute || 0,
+    };
+
+    function formatNum(num) {
+        const numString = num.toString();
+        return numString.length === 1 ? `0${numString}` : numString;
+    }
+
+    function adjustTime(delta) {
+        const newDelta = Math.abs(delta) >= MINUTESPERDAY
+            ? delta % MINUTESPERDAY
+            : delta;
+
+        const currentMinutes = clock.hour * 60 + clock.minute;
+        let newMinutes = (currentMinutes + newDelta) % MINUTESPERDAY;
+
+        newMinutes = newMinutes < 0 ? newMinutes += MINUTESPERDAY : newMinutes;
+
+        clock.hour = Math.floor(newMinutes / 60) % HOURSPERDAY;
+        clock.minute = newMinutes - clock.hour * 60;
+    }
+
+    adjustTime(0);
+
+    return {
+        clock,
+        toString: () => `${formatNum(clock.hour)}:${formatNum(clock.minute)}`,
+        plus(minutes) { adjustTime(minutes); return this; },
+        minus(minutes) { adjustTime(-minutes); return this; },
+        equals: otherClock => (
+            clock.hour === otherClock.clock.hour
+            && clock.minute === otherClock.clock.minute
+        ),
+    };
+}
